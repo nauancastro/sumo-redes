7db7f08651c912a0820129eae596239b282fd78b
Emanuel Pereira <70383474+Emanuel-Pereira121@users.noreply.github.com>
2026-01-09 03:29:07 -0300

    Comentei Um pouco mais pq nunca se sabe

--- high_level_network_handler.gd
+++ high_level_network_handler.gd
@@ -1,5 +1,6 @@
 extends Node
 
+#[.1] = Comentários para deixar as variáveis constantes se tu quiser
 # Para deixar as variáveis constantes, basta descomentar as variáveis abaixo
 # Não esqueça de retirar da função as coisas que pede entre parênteses (esqueci o nome)
 # const IP_ADRESS: String = "127.0.0.1"
@@ -8,12 +9,12 @@ extends Node
 
 var peer: ENetMultiplayerPeer
 
-func start_server(PORT) -> void:
-	peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT, 2)
+func start_server(PORT) -> void: #Retire o PORT AQUI [.1]
+	peer = ENetMultiplayerPeer.new() 
+	peer.create_server(PORT, 2) # Não precisa mexer pra deixar constante a variável
 	multiplayer.multiplayer_peer = peer
 
-func start_client(IP_ADRESS, PORT) -> void:
+func start_client(IP_ADRESS, PORT) -> void: #Retire o PORT e/ou o IP_ADRESS[.1]
 	peer = ENetMultiplayerPeer.new()
-	peer.create_client(IP_ADRESS, PORT)
+	peer.create_client(IP_ADRESS, PORT) # Não precisa mexer pra deixar constante a variável
 	multiplayer.multiplayer_peer = peer
